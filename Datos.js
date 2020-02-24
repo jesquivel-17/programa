15519c3721cdc1b77a64a1ed53c64e9d27c1daa6
Samantha <jesquivel@ithwh.edu.gt>
2020-02-24 14:32:44 -0600

    Se ha agregado funciones a Datos.js

---
+++ Datos.js
@@ -0,0 +1,5 @@
+var dato;
+dato = prompt("Ingrese un numero:");
+document.write("El numero ingresado es: "+dato);
+console.log("El numero ingresado es:"+dato);
+alert("El numero ingresado es: "+dato);
